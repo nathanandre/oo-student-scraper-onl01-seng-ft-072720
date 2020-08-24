c8104071f6cc05948f7b57e68b2cc2e91eea3375
Nathan Burns <nathanandreburns@gmail.com>
2020-08-24 03:07:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+
+require "pry"
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)  
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
